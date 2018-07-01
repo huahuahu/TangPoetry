9bf9c64253ee0ea8e24b1d0f10bd58e2037c423c
tiger <guotenghu@gmail.com>
2018-07-01 22:48:46 +0800

    add tabbar, localization

---
+++ TangPoetry/Util/util.swift
@@ -0,0 +1,13 @@
+//
+//  util.swift
+//  TangPoetry
+//
+//  Created by huahuahu on 2018/7/1.
+//  Copyright © 2018年 huahuahu. All rights reserved.
+//
+
+import Foundation
+
+func localString(_ key: String) -> String {
+    return NSLocalizedString(key, comment: "")
+}
