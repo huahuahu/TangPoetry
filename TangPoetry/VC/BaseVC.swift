c18e85bdfea145bad4700b37cb0fba9f75a96deb
Tiger Guo <teguo@microsoft.com>
2020-05-02 10:35:28 +0800

    autolayout collectionviewcell

---
+++ TangPoetry/VC/BaseVC.swift
@@ -0,0 +1,13 @@
+//
+//  BaseVC.swift
+//  TangPoetry
+//
+//  Created by tigerguo on 2020/5/1.
+//  Copyright © 2020 huahuahu. All rights reserved.
+//
+
+import UIKit
+
+class BaseVC: UIViewController {
+
+}
