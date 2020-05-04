e916b80ea77907c5a3b4fc3d3408b61c1d3dbcdb
Tiger Guo <teguo@microsoft.com>
2020-05-04 10:45:47 +0800

    add drop for imageView

--- TangPoetry/Write/PoetryWriteVC.swift
+++ TangPoetry/Write/PoetryWriteVC.swift
@@ -14,6 +14,7 @@ class PoetryWriteVC: BaseVC {
         imageView.contentMode = .scaleAspectFit
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.backgroundColor = .red
+        imageView.isUserInteractionEnabled = true
         return imageView
     }()
 
@@ -39,12 +40,15 @@ class PoetryWriteVC: BaseVC {
         return label
     }()
 
+    private var imageViewDropDelegate: UIDropInteractionDelegate!
+
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
         let image = UIImage.init(systemName: "square.and.pencil")
         self.tabBarItem = .init(title: "写诗", image: image, selectedImage: image)
         self.navigationItem.title = "写诗"
     }
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -60,6 +64,7 @@ class PoetryWriteVC: BaseVC {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         setupConstraints()
+        setupDragAndDrop()
     }
 
     func setupConstraints() {
@@ -85,4 +90,111 @@ class PoetryWriteVC: BaseVC {
         ]
         NSLayoutConstraint.activate(constraints)
     }
+
+    func setupDragAndDrop() {
+        imageViewDropDelegate = ImageViewDropDelegate(imageView: imageView)
+        let dropInteraction = UIDropInteraction.init(delegate: imageViewDropDelegate)
+
+//        let dropInteraction = UIDropInteraction.init(delegate: self)
+        imageView.addInteraction(dropInteraction)
+    }
+}
+
+// MARK: - Drag & Drop
+//extension PoetryWriteVC: UIDropInteractionDelegate {
+class ImageViewDropDelegate: NSObject, UIDropInteractionDelegate {
+    let imageView: UIImageView
+
+    init(imageView: UIImageView) {
+        dragDropLog("\(#function)")
+        self.imageView = imageView
+        super.init()
+    }
+
+    func updateImages(_ images: [UIImage]) {
+        guard !images.isEmpty else {
+            return
+        }
+        if images.count == 1 {
+            self.imageView.image = images.first
+        } else {
+            self.imageView.animationImages = images
+            self.imageView.animationDuration = 1
+            self.imageView.startAnimating()
+        }
+    }
+
+    func dropInteraction(_ interaction: UIDropInteraction, canHandle session: UIDropSession) -> Bool {
+        dragDropLog("\(#function)")
+        return session.canLoadObjects(ofClass: UIImage.self)
+    }
+
+    func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
+        dragDropLog("\(#function)")
+
+            // Consume drag items (in this example, of type UIImage).
+        session.loadObjects(ofClass: UIImage.self) { imageItems in
+            guard let images = imageItems as? [UIImage] else {
+                return
+            }
+            self.updateImages(images)
+        }
+    }
+
+    func dropInteraction(_ interaction: UIDropInteraction, sessionDidEnter session: UIDropSession) {
+        dragDropLog("\(#function)")
+    }
+
+    func dropInteraction(_ interaction: UIDropInteraction, sessionDidUpdate session: UIDropSession) -> UIDropProposal {
+        dragDropLog("\(#function)")
+        return .init(operation: .copy)
+    }
+
+    func dropInteraction(_ interaction: UIDropInteraction, sessionDidExit session: UIDropSession) {
+        dragDropLog("\(#function)")
+    }
+
+    func dropInteraction(_ interaction: UIDropInteraction, sessionDidEnd session: UIDropSession) {
+        dragDropLog("\(#function)")
+    }
+
+    func dropInteraction(_ interaction: UIDropInteraction, item: UIDragItem, willAnimateDropWith animator: UIDragAnimating) {
+        animator.addAnimations {
+            dragDropLog("\(#function) animator.addAnimations")
+
+            self.imageView.alpha = 0.5
+//            self.imageView.contentMode = .scaleAspectFill
+        }
+        animator.addCompletion { (_) in
+            dragDropLog("\(#function) animator.addCompletion")
+
+            self.imageView.alpha = 1
+        }
+        dragDropLog("\(#function)")
+    }
+
+    func dropInteraction(_ interaction: UIDropInteraction, previewForDropping item: UIDragItem, withDefault defaultPreview: UITargetedDragPreview) -> UITargetedDragPreview? {
+        dragDropLog("\(#function)")
+        let label = UILabel()
+        label.text = "haah"
+        label.frame = .init(origin: .zero, size: .init(width: 100, height: 100))
+//        let preview = UITargetedPreview.init(view: label)
+        let dragPreview = UITargetedDragPreview.init(view: label, parameters: .init(), target: .init(container: self.imageView, center: .zero))
+        return dragPreview
+    }
+
+    func dropInteraction(_ interaction: UIDropInteraction, concludeDrop session: UIDropSession) {
+//        self.imageView.contentMode = .scaleAspectFit
+        dragDropLog("\(#function)")
+    }
+}
+
+extension PoetryWriteVC: UIDragInteractionDelegate {
+    func dragInteraction(_ interaction: UIDragInteraction, itemsForBeginning session: UIDragSession) -> [UIDragItem] {
+        return []
+    }
+}
+
+private func dragDropLog(_ str: String) {
+    print("drag&drop: \(str)")
 }
